a0e0dfbeed096c76994b997eeb93e36cc0daacce
Targaryen <leocj@live.com.co>
2025-09-30 22:08:21 -0500

    about

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,13 @@
 <template>
 <section class="w-full mt-10">
   <div class="mx-auto w-1/2 content-center text-center justify-center">
-    <p class="font-medium text-lg ">Github <a class="text-blue-500" target="_blank" href="https://github.com/Rockhount1793" >@rockhount1793</a></p>
+    <p class="font-medium text-lg ">Github <a class="text-blue-500" target="_blank" href="https://github.com/Mandypick/tierlistvue" >Github - código fuente</a></p>
     <p class="text-gray-300">Tecnologías usadas Vuejs, Tailwindcss y Vite, desplegado en Firebase.</p>
     <p class="text-gray-800 text-xl mt-5">Contacto</p>
 
     <div class="flex items-center space-x-2 justify-center">
       <img class="h-8 w-8" src="../../public/telegram.svg">
-      <p class="font-medium text-lg ">Telegram <a class="text-blue-500" target="_blank" href="https://t.me/rockhount" >@rockhount</a></p>
+      <p class="font-medium text-lg ">Telegram <a class="text-blue-500" target="_blank" href="https://t.me/rockhount" >@Rockhount</a></p>
     </div>
     
     <!--
